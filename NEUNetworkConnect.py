2087e5a69dfb3349bb85550a2bc557763fc5dda7
ForestMrVince <37427388+ForestMrVince@users.noreply.github.com>
2020-05-13 14:04:47 +0800

    Update NEUNetworkConnect.py

--- NEUNetworkConnect.py
+++ NEUNetworkConnect.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import re
 import requests
+
 url="https://ipgw.neu.edu.cn/srun_cas.php?ac_id=1"
 head={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0",
@@ -19,6 +20,7 @@ payload={
     "execution":lt_execution_id[0][1],
     "_eventId":"submit"
 }
+
 r2=sess.post("https://pass.neu.edu.cn/tpass/login?service=https%3A%2F%2Fipgw.neu.edu.cn%2Fsrun_cas.php%3Fac_id%3D1",headers=head,data=payload)
 print(r2.text)
 """
